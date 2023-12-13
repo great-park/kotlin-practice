239f77713b32c2e0814893b423523726933baaa0
great-park <cksgh1735@gmail.com>
2023-12-13 23:14:37 +0900

    lec09: . 을 통해서 getter, setter 호출 가능

--- src/main/kotlin/lec09/Person.kt
+++ src/main/kotlin/lec09/Person.kt
@@ -1,5 +1,13 @@
 package lec09
 
+fun main() {
+    val person = Person("홍길동", 20)
+    println(person.name)
+    println(person.age)
+    person.age = 30
+    println(person.age)
+}
+
 class Person (
     val name: String,
     var age: Int
