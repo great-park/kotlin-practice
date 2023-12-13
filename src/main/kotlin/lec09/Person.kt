bf08836132b033c30012ce17a56e10ea6f81d4cc
great-park <cksgh1735@gmail.com>
2023-12-13 23:21:35 +0900

    lec09: init 블록 - 검증 로직 추가

--- src/main/kotlin/lec09/Person.kt
+++ src/main/kotlin/lec09/Person.kt
@@ -8,7 +8,18 @@ fun main() {
     println(person.age)
 }
 
-class Person (
-    val name: String,
-    var age: Int
-)
+class Person ( // 주 생성자에 파라미터가 없으면, 생략 가능
+    val name: String = "이름 없음", // 디폴트 값
+    var age: Int = 1
+) {
+    // init 블록 : 생성자가 호출될 때 실행되는 블록
+    init {
+        if (age < 0) {
+            throw IllegalArgumentException("나이는 0보다 작을 수 없습니다.")
+        }
+    }
+
+//    // 추가 생성자 (부 생성자) - 디폴트 파라미터 대신 사용 가능
+//    constructor(name: String) : this(name, 1)
+//    constructor(age: Int) : this("이름 없음", 1)
+}
