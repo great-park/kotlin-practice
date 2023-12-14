fb21a9b1a9c06c012b9f6fc9a3de6d851178c437
great-park <cksgh1735@gmail.com>
2023-12-15 01:47:06 +0900

    lec19: label

--- src/main/kotlin/lec19/Main.kt
+++ src/main/kotlin/lec19/Main.kt
@@ -16,4 +16,14 @@ fun main() {
     val (name, age) = person // 구조 분해 선언: componentN() 함수를 호출하여 변수에 값을 할당하는 것
     // name = person.component1()
     // age = person.component2()
+
+    // 라벨 기능
+    // 안 쪽 반복문이 멈추지 않고 라벨이 붙은 바깥 쪽 반복문 멈춘다.
+    abc@ for (i in 1..100) {
+        for (j in 1..100) {
+            if (j == 2) {
+                break@abc
+            }
+        }
+    }
 }
