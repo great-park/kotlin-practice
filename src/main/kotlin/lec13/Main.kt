abeb5359c5c199d8a877ab44d30d84f422880626
great-park <cksgh1735@gmail.com>
2023-12-15 00:25:16 +0900

    lec13: inner -> 바깥 클래스를 참조하는 내부 클래스를 만들 때 사용, this@ 사용

--- src/main/kotlin/lec13/Main.kt
+++ src/main/kotlin/lec13/Main.kt
@@ -11,3 +11,16 @@ class House(
         private var area: Double
     )
 }
+
+class House2(
+    var address: String
+) {
+    var livingRoom = this.LivingRoom(10.0)
+
+    inner class LivingRoom(
+        private var area: Double
+    ) {
+        val address: String
+            get() = this@House2.address
+    }
+}
