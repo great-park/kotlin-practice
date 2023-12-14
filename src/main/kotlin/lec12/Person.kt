44002c2875bec4df8bec4e6ea00620dfaea6db03
great-park <cksgh1735@gmail.com>
2023-12-15 00:06:08 +0900

    lec12: const 키워드 -> 컴파일시 할당

--- src/main/kotlin/lec12/Person.kt
+++ src/main/kotlin/lec12/Person.kt
@@ -5,7 +5,7 @@ class Person private constructor(
     var age: Int
 ) {
         companion object {
-            private val MIN_AGE: Int = 1
+            private const val MIN_AGE: Int = 1
             fun newBaby(name: String, age: Int): Person {
                 return Person(name, MIN_AGE)
             }
