c904f20247091bd5dfe5e8e3edca51262fdedd09
great-park <cksgh1735@gmail.com>
2023-12-15 01:28:37 +0900

    lec18: 람다를 1급 시민으로 사용하기 위해 Closure 사용

--- src/main/kotlin/lec18/Main.kt
+++ src/main/kotlin/lec18/Main.kt
@@ -20,6 +20,13 @@ fun main() {
     filterFruits(fruits, isApple).forEach { println(it) } // 람다를 파라미터로!
 
     // 코틀린에서는 함수가 1급 시민이다. 자바에서는 2급 시민임
+
+    // 클로저
+    // 코틀린에서는 람다가 시작하는 지점에 참조하고 있는 변수들을 모두 포획하여 그 정보를 가지고 있다.
+    // 이렇게 해야만, 람다를 진정한 일급 시민으로 간주할 수 있다. 이러한 데이터 구조를 클로저라고 부른다.
+    var targetFruitName = "바나나"
+    targetFruitName = "오렌지"
+    filterFruits(fruits) { it.name == targetFruitName }
 }
 
 private fun filterFruits(
