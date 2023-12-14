5d7920083bda761f425b420f6bee94a0558f558d
great-park <cksgh1735@gmail.com>
2023-12-15 01:41:25 +0900

    lec19: 구조 분ˆ - componentN()

--- src/main/kotlin/lec19/Main.kt
+++ src/main/kotlin/lec19/Main.kt
@@ -5,3 +5,15 @@ import lec18.Fruit as F // as import
 typealias FruitFilter = (F) -> Boolean // 타입 별칭
 fun filterFruits(fruits: List<F>, filter: FruitFilter) {
 }
+
+data class Person(
+    val name: String,
+    val age: Int,
+)
+
+fun main() {
+    val person = Person("홍길동", 20)
+    val (name, age) = person // 구조 분해 선언: componentN() 함수를 호출하여 변수에 값을 할당하는 것
+    // name = person.component1()
+    // age = person.component2()
+}
