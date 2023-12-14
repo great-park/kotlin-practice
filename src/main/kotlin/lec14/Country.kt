66070ae2f0d8c358255487bcb69163275ee86724
great-park <cksgh1735@gmail.com>
2023-12-15 00:40:27 +0900

    lec14: sealed class - Enum과 같이 컴파일 타임 떄 하위 클래스의 타입을 모두 기억한다. 하지만 Enum과 달리 클래스 상속이 가능하며 멀티 인스턴스가 가능하다.

--- src/main/kotlin/lec14/Country.kt
+++ src/main/kotlin/lec14/Country.kt
@@ -7,6 +7,23 @@ fun handleCountry(country: Country) {
     }
 }
 
+fun handleCar(car: Car) {
+    when (car) {
+        is Avante -> println("아반떼")
+        is Sonata -> println("소나타")
+        is Genesis -> println("제네시스")
+    }
+}
+
+sealed class Car(
+    val name: String,
+    val price: Long,
+)
+
+class Avante: Car("아반떼", 10000000L)
+class Sonata: Car("소나타", 20000000L)
+class Genesis: Car("제네시스", 30000000L)
+
 enum class Country(
     private val codee: String,
 ) {
