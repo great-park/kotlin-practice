6d30fd7beb796fe2a84fc4f13004e90dd67f5d07
great-park <cksgh1735@gmail.com>
2023-12-15 02:14:52 +0900

    lec20: 용례 추가

--- src/main/kotlin/lec20/Main.kt
+++ src/main/kotlin/lec20/Main.kt
@@ -24,6 +24,39 @@ fun main(){
             if(firstItem.length >= 5) firstItem else "!$firstItem!"
         }.uppercase()
     println(modifiedFirstItem)
+
+    // run: 객체 초기화와 반환 값의 계산을 동시에 해야 할 때
+    val person = Person("홍길동", 20).run {
+        // 바로 실시한 계산 로직 추가
+        // ex) 객체를 만들어 DB에 바로 저장하고, 그 인스턴스를 활용할 때 - this 사용
+        }
+
+    // apply, also : 객체 설정을 할 때에 객체를 수정하는 로직이 call chain 중간에 필요할 때
+    fun createPerson(
+        name: String,
+        age: Int,
+        address: String
+    ): Person2 {
+        return Person2(name, age).apply {
+            this.address = address
+        }
+    }
+
+    mutableListOf("one", "two", "three")
+        .also {println("four 추가 이전 지금 값: $it")}
+        .add("four")
+
+
+    // with: 특정 객체를 다른 객체로 변환해야 하는데, 모듈 간의 의존성에 의해서 정적 팩토리 혹은 toClass 함수를 만들기 어려울 때
+
+
+}
+
+data class Person2(
+    var name: String,
+    var age: Int,
+) {
+    var address: String? = null
 }
 
 // scope function : 람다를 사용해 일시적인 영역을 만들고, 코드를 간결하게 만들거나 method chaining을 할 수 있게 해준다.
