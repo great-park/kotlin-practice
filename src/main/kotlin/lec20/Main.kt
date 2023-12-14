1b5f99e1d62dff3717d476fa6c6ef647c77ef363
great-park <cksgh1735@gmail.com>
2023-12-15 01:56:22 +0900

    lec20: scope function

--- src/main/kotlin/lec20/Main.kt
+++ src/main/kotlin/lec20/Main.kt
@@ -7,6 +7,10 @@ fun main(){}
 // scope function : 람다를 사용해 일시적인 영역을 만들고, 코드를 간결하게 만들거나 method chaining을 할 수 있게 해준다.
 // apply, run, with, also, let
 // let : 수신 객체를 람다의 인자로 전달하고 람다의 본문을 실행한 결과를 반환
+// run : 수신 객체를 람다의 수신 객체로 전달하고 람다의 본문을 실행한 결과를 반환
+// apply : 수신 객체를 람다의 수신 객체로 전달하고 수신 객체 자체를 반환
+// also : 수신 객체를 람다의 인자로 전달하고 수신 객체 자체를 반환
+// with : 수신 객체를 람다의 수신 객체로 전달하고 람다의 본문을 실행한 결과를 반환
 fun printPerson(person: Person?) {
     person?.let { // Safe Call: person이 null이 아닐 때만 let 호출
         println(it.name)
