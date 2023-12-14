fc705e135c5b2c018495f7701308efe011dd582b
great-park <cksgh1735@gmail.com>
2023-12-15 01:10:10 +0900

    lec16: inline 함수, 지역 함수

--- src/main/kotlin/lec17/Main.kt
+++ src/main/kotlin/lec17/Main.kt
@@ -21,3 +21,14 @@ fun String.lastChar(): Char { // 수신객체
 infix fun Int.add2(other: Int): Int {
     return this + other
 }
+
+// inline 함수
+// 함수를 호출하는 곳에 함수 본문을 그대로 복붙해준다.
+// 즉 함수를 호출하는 것이 아니라 로직 자체가 그대로 옮겨진다. => 함수를 파라미터로 전달할 때 오버헤드를 줄일 수 있다.
+inline fun Int.add3(b: Int): Int {
+    return this + b
+}
+
+// 지역 함수
+// 함수 안에 함수를 정의할 수 있다.
+// 함수로 추출하면 좋을 것 같은데, 이 함수를 다른 곳에서 사용할 일이 없을 때 사용한다.
