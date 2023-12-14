4946d9327a318b39b9b904b9d22e0533fd474212
great-park <cksgh1735@gmail.com>
2023-12-15 01:03:05 +0900

    lec16: 확장 함 - 캡슐화를 지키기 위해 확장 함수는 클래스에 있는 private, protected 멤버에 접근할 수 없음, 시그니처가 같으면 멤버 함수가 우선적으로 호출된다, 오버라이드될 경우, 해당 변수의 현재 타입 즉, 정적인 타입에 의해 어떤 확장 함수가 호출될지 결정된다.

--- src/main/kotlin/lec17/Main.kt
+++ src/main/kotlin/lec17/Main.kt
@@ -6,6 +6,9 @@ fun main() {
 }
 
 // 확장 함수: 코드는 클래스 밖에서 작성하고, 마치 내부 함수인 것처럼 사용할 수 있음
-fun String.lastChar(): Char {
+// 캡슐화를 지키기 위해 확장 함수는 클래스에 있는 private, protected 멤버에 접근할 수 없음
+// 시그니처가 같으면 멤버 함수가 우선적으로 호출된다.
+// 오버라이드될 경우, 해당 변수의 현재 타입 즉, 정적인 타입에 의해 어떤 확장 함수가 호출될지 결정된다.
+fun String.lastChar(): Char { // 수신객체
     return this[this.length - 1]
 }
