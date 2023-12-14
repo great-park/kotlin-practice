062ffd7362ba4ad851f33455f5fd0c3361750d23
great-park <cksgh1735@gmail.com>
2023-12-15 02:03:54 +0900

    lec20: let 용례

--- src/main/kotlin/lec20/Main.kt
+++ src/main/kotlin/lec20/Main.kt
@@ -2,7 +2,29 @@ package lec20
 
 import lec09.Person
 
-fun main(){}
+fun main(){
+
+    // let: 하나 이상의 함수를 call chain 결과로 호출할 때
+    val strings = listOf("Apple", "Banana", "Orange", "Pear", "Grape", null)
+    strings.map { it?.length ?: 0 }
+        .filter { it > 3 }
+        .let(::println)
+
+    // let: non-null 값에 대해서만 code block을 실행시킬 때
+    val length = strings?.let {
+        println(it)
+        it.size
+    }
+
+    // let: 일회성으로 제한된 영역에 지역 변수를 만들 때
+    val numbers = listOf("one", "two", "three", "four")
+    val modifiedFirstItem = numbers.first()
+        .let {
+            firstItem ->
+            if(firstItem.length >= 5) firstItem else "!$firstItem!"
+        }.uppercase()
+    println(modifiedFirstItem)
+}
 
 // scope function : 람다를 사용해 일시적인 영역을 만들고, 코드를 간결하게 만들거나 method chaining을 할 수 있게 해준다.
 // apply, run, with, also, let
